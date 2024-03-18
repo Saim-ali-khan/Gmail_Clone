fbf72c7075656a964304c3e255508be1c60465d9
Saim ALi <saimk1588@gmail.com>
2024-03-18 14:30:56 +0500

    Mail section added

--- js/index.js
+++ js/index.js
@@ -11,8 +11,8 @@ document.getElementById("srh").addEventListener("focusout", function (e) {
     console.log(this.parentElement.parentElement)
 });
 
-var el=document.getElementById("tos");
-el.setAttribute("id","non");
+var el = document.getElementById("tos");
+el.setAttribute("id", "non");
 
 var ml = 0;
 document.getElementById("options").children[5].addEventListener("click", function () {
@@ -22,7 +22,7 @@ document.getElementById("options").children[5].addEventListener("click", functio
 
         this.children[1].innerHTML = "<div>less</div>";
         console.log(el);
-        el.setAttribute("id","rel");
+        el.setAttribute("id", "rel");
         console.log(el);
         ml = 1;
     }
@@ -30,10 +30,89 @@ document.getElementById("options").children[5].addEventListener("click", functio
         this.children[0].setAttribute("class", "cin3");
 
         this.children[1].innerHTML = "<div>More</div>";
-        el.setAttribute("id","non");
-        ml= 0;
+        el.setAttribute("id", "non");
+        ml = 0;
 
 
     }
 
 });
+
+let el1 = document.getElementsByClassName("cat1")[0];
+el1.style.borderBottom = " 4px solid blue";
+
+
+function createNewMail(data) {
+
+    let add = document.createElement("div");
+    add.setAttribute("class", "mal1");
+
+    let star = document.createElement("div");
+    star.setAttribute("class", "starr");
+    star.innerHTML = `<i class="fa-regular fa-star"></i>`;
+
+
+    add.appendChild(star);
+
+    let frm = document.createElement("div");
+    frm.setAttribute("class", "from");
+    frm.innerText = data.name;
+
+    add.appendChild(frm);
+
+
+
+    let sub = document.createElement("div");
+    sub.innerHTML = data.subject;
+
+    add.appendChild(sub);
+
+
+    let date = document.createElement("div");
+    date.innerHTML = data.date;
+    date.setAttribute("class", "date");
+    add.appendChild(date);
+
+
+
+    let opt = document.createElement("div");
+    opt.setAttribute("class", "onopt");
+    opt.innerHTML = `
+<div><i class="fa-solid fa-box-archive"></i></div>
+<div><i class="fa-solid fa-trash-can"></i></div>
+<div><i class="fa-regular fa-envelope"></i></div>
+<div><i class="fa-regular fa-clock"></i></div>
+`;
+    add.appendChild(opt);
+    return add;
+}
+
+
+let person = {
+    name: "Instagram and Google are trying",
+    subject: "New developer hired by the google",
+    date: "June 17"
+};
+
+for (let i = 0; i < 2; i++) {
+    let n1 = createNewMail(person);
+    let mail = document.getElementById("maills");
+    mail.appendChild(n1);
+}
+
+function tooltip(ele,str){
+    let ell=document.createElement("div");
+    ell.setAttribute("class","tip");
+    ell.innerHTML=str;
+    ele.appendChild(ell);
+}
+
+
+let ti=document.getElementsByClassName("starr");
+for(let i=0;i<ti.length;i++){
+
+    tooltip(ti[i],"Starred");
+}
+
+
+
